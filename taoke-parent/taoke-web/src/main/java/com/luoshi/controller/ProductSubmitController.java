c4dc1d1e215cc89768e9d7607867dd9a37c23ad3
chenzj11211 <787535187@qq.com>
2019-10-20 23:49:04 +0800

    修改接口

--- taoke-parent/taoke-web/src/main/java/com/luoshi/controller/ProductSubmitController.java
+++ taoke-parent/taoke-web/src/main/java/com/luoshi/controller/ProductSubmitController.java
@@ -1,9 +1,20 @@
 package com.luoshi.controller;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URLEncoder;
 import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.luoshi.pojo.TbProductSubmit;
@@ -111,5 +122,32 @@ public class ProductSubmitController {
 	public PageResult search(@RequestBody TbProductSubmit productSubmit, int page, int rows  ){
 		return productSubmitService.findPage(productSubmit, page, rows);		
 	}
+	
+	/**
+	 * 文件下载
+     * @throws IOException 
+     */
+    @RequestMapping(value="/download")
+    public void download(@RequestParam(value="filename")String filename,
+       HttpServletRequest request,
+       HttpServletResponse response) throws IOException {
+        //模拟文件，myfile.txt为需要下载的文件  
+        String path = request.getSession().getServletContext().getRealPath("statics\\moban")+"\\"+filename;  
+        //获取输入流  
+        InputStream bis = new BufferedInputStream(new FileInputStream(new File(path)));
+        //转码，免得文件名中文乱码  
+        filename = URLEncoder.encode(filename,"UTF-8");  
+        //设置文件下载头  
+        response.addHeader("Content-Disposition", "attachment;filename=" + filename);    
+        //1.设置文件ContentType类型，这样设置，会自动判断下载文件类型    
+        response.setContentType("multipart/form-data");   
+        BufferedOutputStream out = new BufferedOutputStream(response.getOutputStream());  
+        int len = 0;  
+        while((len = bis.read()) != -1){  
+            out.write(len);  
+            out.flush();  
+        }  
+        out.close();  
+    }
 	
 }
